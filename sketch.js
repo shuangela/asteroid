12ab579870ca00a67247e393d67d4f148055fe41
shuangela <angelashu103@gmail.com>
2019-07-26 00:38:20 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -111,7 +111,7 @@ function createAstroid() {
   var a = createSprite(astroidX, astroidH, 150, 150);
   var img = loadImage('https://i.imgur.com/cEZZLFv.png');
   a.addImage(img);
-  astroids.addToGroup(a);
+  a.addToGroup(astroids);
   return a;
   //drawSprite(a);
   //a.setSpeed(2.5-(type/2), random(360));
